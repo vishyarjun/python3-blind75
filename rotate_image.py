3b342ab29384e14617b8216d8861a4e83d180134
Arjun Viswanathan <viswanathan.arjun@gmail.com>
2022-06-08 19:48:23 -0400

    Update rotate_image.py

--- rotate_image.py
+++ rotate_image.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        l,r = 0,len(matrix)-1
+        while l<r:
+            t,b = l,r
+            for i in range(r-l):
+                temp = matrix[t][l+i]
+                matrix[t][l+i] = matrix[b-i][l]
+                matrix[b-i][l] = matrix[b][r-i]
+                matrix[b][r-i] = matrix[t+i][r]
+                matrix[t+i][r] = temp
+            l+=1
+            r-=1
